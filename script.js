d18262e1b834ef473ec044c6a950f0b535fc28a4
Rudransh Pathak <127862704+rudranshpathak@users.noreply.github.com>
2023-06-17 22:49:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
 
-
+const boldWords = document.getElementsByTagName("strong");
+  for (let i = 0; i < boldWords.length; i++) {
+    boldWords[i].style.color = "green";
+  }
 }
 
 
 function return_normal() {
     //Write your code here
-
+ const boldWords = document.getElementsByTagName("strong");
+  for (let i = 0; i < boldWords.length; i++) {
+    boldWords[i].style.color = "black";
+  }
     
 }
